1477cd8a2f9c0ca42faac765c3b519915407018f
Vikrant V Joliya <vikrant.v.joliya@gmail.com>
2023-08-27 20:40:54 +0530

    Added Models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(maxlength=50)
+    last_name = models.CharField(maxlength=50)
+    email = models.CharField(maxlength=100)
+    address = models.CharField(maxlength=10)
+    city = models.CharField(maxlength=50)
+    state = models.CharField(maxlength=10)
+    zipcode = models.CharField(maxlength=20)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
